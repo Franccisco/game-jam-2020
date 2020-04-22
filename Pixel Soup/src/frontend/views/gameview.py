335d3503f75ee1dbc0f341da1715631a4ab3d07b
Franccisco <35090042+Franccisco@users.noreply.github.com>
2020-04-21 23:54:52 -0400

    Add player_list to GameView

--- Pixel Soup/src/frontend/views/gameview.py
+++ Pixel Soup/src/frontend/views/gameview.py
@@ -4,14 +4,21 @@ from .pauseview import PauseView
 
 
 class GameView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        # Lists that will keep track of the sprites
+        self.player_list = None
+
     def on_show(self) -> None:
         arcade.set_background_color(arcade.color.WHITE)
 
     def on_draw(self) -> None:
         arcade.start_render()
 
+        self.player_list.draw()
+
     def setup(self) -> None:
-        pass
+        self.player_list = arcade.SpriteList()
 
     def pause_game(self) -> None:
         """Switch to a Pause view."""
