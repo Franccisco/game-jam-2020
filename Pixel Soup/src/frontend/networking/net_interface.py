f377eb04695293ba232cb75f4b05753b51b518fa
Inventor <1inventordavid@gmail.com>
2020-04-25 22:07:29 +0100

    optimising net_interface

--- Pixel Soup/src/frontend/networking/net_interface.py
+++ Pixel Soup/src/frontend/networking/net_interface.py
@@ -13,7 +13,6 @@ class Pipe:
         self.udp_password = None
         self.game_port = None
 
-    # to connect to the backend server
     def connect(self) -> bool:
         try:
             self.tcp.connect((self.server, self.port))
@@ -22,7 +21,6 @@ class Pipe:
         except (ConnectionResetError, ConnectionRefusedError):
             return False
 
-    # this handles player log in entry point
     def login(self, entry: str, room_name: str, username: str) -> tuple:
         self.username = username
         if not self.connected:
@@ -31,7 +29,6 @@ class Pipe:
                 return False, "error in connection"
 
         self.tcp.send(f"{entry},,{room_name},,{username}".encode())
-        # receives login status of the player
         response = self.tcp.recv(100)
 
         if entry == "create":
@@ -45,18 +42,15 @@ class Pipe:
                 return False, "invalid"
 
         else:
-            # when room name is not found
             if response == b"Missing":
                 return False, "missing"
 
-            # when team is full
             elif response == b"Full":
                 return False, "full"
 
             else:
                 return True, "joined"
 
-    # receives team status and game room parameters
     def await_response(self) -> list:
         try:
             data = self.tcp.recv(1000)
@@ -70,21 +64,20 @@ class Pipe:
         except ConnectionResetError:
             return [False]
 
-    # transports game data to the server via udp and
-    # also receives other players game data
     def transport(self, game_data: list) -> tuple:
         game_data = [self.udp_password, self.username] + game_data
 
         game_data = (dumps(game_data)) + b"||||"
+        print(f"{len(game_data)} length of data")
 
         self.udp.sendto(game_data, (self.server, self.game_port))
 
-        data, _ = self.udp.recvfrom(2000)
+        data, _ = self.udp.recvfrom(100)
+        print(f"receiving {len(data)}")
         data = data.split(b"||||")
 
         if len(data) > 1:
-            data = data[0].split(b"||")
-            data = [loads(game) for game in data]
+            data = loads(data[0])
 
             return True, data
         else:
